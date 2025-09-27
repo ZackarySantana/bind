3cd95ff79d6e3a0adfba56f32badcec820a02bf6
ZackarySantana <64446617+ZackarySantana@users.noreply.github.com>
2025-09-27 15:45:10 -0400

    feat(valkey): start on module

---
+++ modules/valkey/go.mod
@@ -0,0 +1,10 @@
+module github.com/zackarysantana/bind/modules/valkey
+
+go 1.24.5
+
+require github.com/zackarysantana/bind v0.0.0-20250925035000-e4da6820f86b
+
+require (
+	github.com/valkey-io/valkey-glide/go/v2 v2.1.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
+)
---
+++ modules/valkey/valkey.go
@@ -0,0 +1,19 @@
+package valkey
+
+import (
+	"context"
+
+	glide "github.com/valkey-io/valkey-glide/go/v2"
+	"github.com/zackarysantana/bind"
+)
+
+const (
+	TagValKey = "valkey"
+)
+
+func NewSupplier[T any](client *glide.Client, ref any) (bind.Supplier, error) {
+	return bind.NewSelfSupplier(func(ctx context.Context, filter map[string]any) (T, error) {
+		var out T
+		return out, client.Get(ctx, filter).Decode(&out)
+	}, TagValKey, ref)
+}
